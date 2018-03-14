5048af94bb2cec016928c7ba47992208411a23bd
p13mark1 <p13mark1@ionio.gr>
2018-03-14 23:21:35 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -145,6 +145,15 @@ function total() {
 		.start();
 }
 
+function amountType() {
+	
+	force.gravity(0)
+		.friction(0.85)
+		.charge(function(d) { return -Math.pow(d.radius, 2) / 2.5; })
+		.on("tick", amounts)
+		.start();
+}
+
 function partyGroup() {
 	force.gravity(0)
 		.friction(0.8)
